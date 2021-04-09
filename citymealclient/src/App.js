896439dce043be0bdf99db4dc4b9b0d4e144b39a
Yemissy <yonayemi@yahoo.com>
2021-04-09 11:17:32 -0400

    made a component react test'

--- citymealclient/src/App.js
+++ citymealclient/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Nav from './Component/Navbar/Navbar'
+import Nav from './Component/Navbar/navbar'
 
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <h1>Hello World</h1>
       <button onClick={updateNav}>Click to update nav</button>
       <Nav size={screenSize} />
     </div>
--- citymealclient/src/Component/Navbar/Navbar.js
+++ citymealclient/src/Component/Navbar/Navbar.js
@@ -1,13 +1,26 @@
 import React from 'react';
-
+import './navbar.css'
 function Nav(props) {
- 
+  console.log(props.size)
   ///After user signs in, update nav content
   return (
     <div className="Nav">
       {props.size > 360 ? 
-        <Nav >Nav greater than screenSize</Nav> : 
-        <Nav>Hello</Nav>
+        <div id='navish'>
+          <ul>
+            <li>Home</li>
+            <li>Favorites</li>
+            <li>List</li>
+          </ul> 
+        </div> : 
+        <div id="dropdown">
+          <select>
+            <option>Home</option>
+            <option>Missy</option>
+            <option>Seola's View</option>
+          </select>
+      </div> 
+           
       }
     </div>
   );
