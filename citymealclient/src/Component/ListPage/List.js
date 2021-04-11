e7b703e2bb724cd7c1401dfcf8972b2481e477c4
seolacarrano <seola6@gmail.com>
2021-04-11 13:32:43 -0400

    updated list css

--- citymealclient/src/Component/ListPage/List.js
+++ citymealclient/src/Component/ListPage/List.js
@@ -4,17 +4,20 @@ import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
-  input: {
-    paddingBottom: "5%",
-    [theme.breakpoints.down('sm')]: {
-      marginLeft: '30%',
-    },
-    [theme.breakpoints.up('md')]: {
-      marginLeft: '40%',
-    },
-    [theme.breakpoints.up('lg')]: {
-      marginLeft: '40%',
-    },
+  zipcodeInput: {
+    // paddingBottom: "5%",
+    // [theme.breakpoints.down('sm')]: {
+    //   marginLeft: '40%',
+    // },
+    // [theme.breakpoints.up('md')]: {
+    //   marginLeft: '40%',
+    // },
+    // [theme.breakpoints.up('lg')]: {
+    //   marginLeft: '40%',
+    // },
+    display: 'flex',
+    justifyContent: 'center',
+    paddingBottom: '5%',
   },
   list: {
     display: 'flex',
@@ -26,6 +29,9 @@ const useStyles = makeStyles((theme) => ({
   pic: {
     display: 'flex',
     height: '15rem',
+    paddingTop: '0.5rem',
+    paddingBottom: '0.5rem',
+    paddingLeft: '0.5rem',
   },
   info: {
     display: 'flex',
@@ -45,8 +51,8 @@ function EachList() {
   const classes = useStyles();
 
   return (
-    <Box className={classes.list} maxWidth="xl" border={1}>
-      <Box className={classes.pic} width={1 / 2}><img height="100%" width="100%" src="https://res.cloudinary.com/dqduwnrb1/image/upload/v1618158659/GoogleMapTA_nkou2y.jpg"></img></Box>
+    <Box className={classes.list} maxWidth="xl" border={1} borderRadius={16}>
+      <Box className={classes.pic} borderRadius={16} width={1 / 2}><img height="auto" width="100%" padding-left="3%" src="https://res.cloudinary.com/dqduwnrb1/image/upload/v1618158659/GoogleMapTA_nkou2y.jpg" alt="map" /></Box>
       <Box className={classes.info} width={1 / 2}>
         {/* <Box className={classes.eachInfo}>Franklin Delano Roosevelt High School - 5800 20 Avenue, 11204</Box>
         <Box className={classes.eachInfo}>D, E, 2, 3</Box>
@@ -66,7 +72,7 @@ function List() {
 
   return (
     <div >
-      <TextField className={classes.input} id="standard-search" label="zipcode" type="search" />
+      <div className={classes.zipcodeInput}><TextField id="standard-search" label="zipcode" type="search" /></div>
       <EachList />
       <EachList />
       <EachList />
